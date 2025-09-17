f7d9dbe26a80229236c31f36c2be98571c991a10
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-17 18:36:29 +0100

    multiple card created  and pillfile updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,8 +3,10 @@ import Card from "@/components/Card"
 
 const Landing: React.FC =  () => {
   return (
-    <div>
+    <div className="display-grid">
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+     <Card />
+     <Card />
      <Card />
 
     </div>
